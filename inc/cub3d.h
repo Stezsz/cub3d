5246b24ba6a6f8c3aae8333634d27997130742fa
Tiago Leite <tiagobscleite@gmail.com>
2025-05-25 23:42:43 +0100

    janela preta ✅

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,15 +6,18 @@
 /*   By: tborges- <tborges-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 22:11:07 by tborges-          #+#    #+#             */
-/*   Updated: 2025/05/25 23:21:21 by tborges-         ###   ########.fr       */
+/*   Updated: 2025/05/25 23:29:55 by tborges-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <mlx.h>
+# define WIDTH 1280
+# define HEIGHT 720
+
 # include <math.h>
+# include <mlx.h>
 # include <stdbool.h>
 # include <stdlib.h>
 # include <unistd.h>
@@ -24,6 +27,11 @@ typedef struct s_game
 	void	*mlx;
 	void	*win;
 	void	*img;
+
+	char	*data;
+	int		bpp;
+	int		size_line;
+	int		endian;
 }			t_game;
 
 #endif
--- src/cub3d.c
+++ src/cub3d.c
@@ -6,17 +6,38 @@
 /*   By: tborges- <tborges-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 22:10:58 by tborges-          #+#    #+#             */
-/*   Updated: 2025/05/25 23:22:35 by tborges-         ###   ########.fr       */
+/*   Updated: 2025/05/25 23:42:20 by tborges-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
 
+void put_pixel(int x, int y, int color, t_game *game)
+{
+	if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+		return ;
+	int index = y * game->size_line + x * game->bpp / 8;
+	game->data[index] = color & 0xFF; // Blue
+	game->data[index + 1] = (color >> 8) & 0xFF; // Green
+	game->data[index + 2] = (color >> 16) & 0xFF; // Red
+}
+
+void draw_square(int x, int y, int size, int color, t_game *game)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			put_pixel(x + j, y + i, color, game);
+		}
+	}
+}
+
 void	init_game(t_game *game)
 {
 	game->mlx = mlx_init();
-	game->win = mlx_new_window(game->mlx, 1280, 720, "Game");
-	game->img = mlx_new_image(game->mlx, 1280, 720);
+	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "Game");
+	game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
 }
 
 int main(void)
@@ -24,6 +45,7 @@ int main(void)
 	t_game game;
 
 	init_game(&game);
+	// draw_square(WIDTH / 2, HEIGHT / 2, 10, 0xFF0000, &game); // Draw a red square at (100, 100) with size 50
 	mlx_loop(game.mlx);
 
 	return (0);
